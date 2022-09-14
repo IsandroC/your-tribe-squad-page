36e4f16a66461aed9ed1d57d62b2ae3d0a230cc3
SanHont <dehontsander@gmail.com>
2022-09-14 12:12:53 +0200

    Format verbeterd

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-    $(".home").hover(
-        function() {$(this).attr("src","assets/luckyblockselection.gif");},
-        function() {$(this).attr("src","assets/luckyblock.png");
-    });
+$(document).ready(function () {
+  $(".home").hover(
+    function () {
+      $(this).attr("src", "assets/luckyblockselection.gif");
+    },
+    function () {
+      $(this).attr("src", "assets/luckyblock.png");
+    }
+  );
 });
--- scripts/squad.js
+++ scripts/squad.js
@@ -1,5 +1,5 @@
 function openBox() {
-    if (document.getElementById("mysteryBox").src == "/assets/boxes dicht.png") {
-        document.getElementById("mysteryBox").src = "/assets/boxes open.png "
-    }
+  if (document.getElementById("mysteryBox").src == "/assets/boxes dicht.png") {
+    document.getElementById("mysteryBox").src = "/assets/boxes open.png ";
+  }
 }
