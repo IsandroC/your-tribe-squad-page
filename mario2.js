37533cccf124a0f235f564d064bb31b36e4d0766
SanHont <dehontsander@gmail.com>
2022-09-16 11:13:44 +0200

    Verbeterde versie

---
+++ mario2.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".home").hover(
+    function () {
+      $(this).attr("src", "assets/luckyblockselection.gif");
+    },
+    function () {
+      $(this).attr("src", "assets/luckyblock.png");
+    }
+  );
+});
