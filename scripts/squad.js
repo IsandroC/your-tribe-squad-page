d14a608bf5d7c6bfacba11f4a590b1daccf612b8
SanHont <dehontsander@gmail.com>
2022-09-19 10:29:56 +0200

    Pagina's verwisseld en aangepast

--- scripts/squad.js
+++
@@ -1,5 +0,0 @@
-function openBox() {
-  if (document.getElementById("mysteryBox").src == "/assets/boxes dicht.png") {
-    document.getElementById("mysteryBox").src = "/assets/boxes open.png ";
-  }
-}
